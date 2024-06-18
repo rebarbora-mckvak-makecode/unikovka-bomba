78961b090ce91df042e67005392df8400393c72d
rebarbora-mckvak-makecode <rebarbora.mckvak+makecode@gmail.com>
2024-06-18 12:18:08 +0200

    hlasitost

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+"""
+
+modra sviti
+zluta sviti
+oranzova sviti
+fialova nesviti
+seda nesviti
+zelena nesviti
+cerna nesviti
+cervena nesviti
+
+"""
+
 def on_button_press(index):
-    tm1638.setLED_on(index)
+    if ledky[index] == 0:
+        ledky[index] = 1
+        tm1638.setLED_on(index)
+    else:
+        ledky[index] = 0
+        tm1638.setLED_off(index)
 tm1638.on_button_press_event(on_button_press)
 
-sekund_do_vybuchu = 10
+def vytvor_seznam_ledky():
+    global ledky
+    ledky = []
+    for pořadí in range(9):
+        ledky[pořadí] = 0
+ledky: List[number] = []
+vytvor_seznam_ledky()
+sekund_do_vybuchu = 600
 tm1638.init(DigitalPin.P0, DigitalPin.P1, DigitalPin.P2)
-while sekund_do_vybuchu > 0:
+while not (sekund_do_vybuchu == 0 or ledky[1] == 0 and ledky[2] == 0 and ledky[3] == 0 and ledky[4] == 1 and ledky[5] == 0 and ledky[6] == 1 and ledky[7] == 1 and ledky[8] == 0):
     sekund_do_vybuchu += -1
     basic.pause(1000)
     music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
@@ -18,7 +43,3 @@ basic.pause(1000)
 basic.clear_screen()
 while True:
     basic.show_string("game over")
-
-def on_forever():
-    pass
-basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,9 +1,32 @@
+// modra sviti
+// zluta sviti
+// oranzova sviti
+// fialova nesviti
+// seda nesviti
+// zelena nesviti
+// cerna nesviti
+// cervena nesviti
 tm1638.onButtonPressEvent(function (index) {
-    tm1638.setLED_on(index)
+    if (ledky[index] == 0) {
+        ledky[index] = 1
+        tm1638.setLED_on(index)
+    } else {
+        ledky[index] = 0
+        tm1638.setLED_off(index)
+    }
 })
-let sekund_do_vybuchu = 10
+function vytvor_seznam_ledky () {
+    ledky = []
+    for (let pořadí = 0; pořadí <= 8; pořadí++) {
+        ledky[pořadí] = 0
+    }
+}
+let ledky: number[] = []
+music.setVolume(60)
+vytvor_seznam_ledky()
+let sekund_do_vybuchu = 600
 tm1638.init(DigitalPin.P0, DigitalPin.P1, DigitalPin.P2)
-while (sekund_do_vybuchu > 0) {
+while (!(sekund_do_vybuchu == 0 || ledky[1] == 0 && ledky[2] == 0 && ledky[3] == 0 && ledky[4] == 1 && ledky[5] == 0 && ledky[6] == 1 && ledky[7] == 1 && ledky[8] == 0)) {
     sekund_do_vybuchu += -1
     basic.pause(1000)
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
